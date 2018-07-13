345d60d78697719ba55639970ca209f04f39f9e5
谢烨 <hatehate123@126.com>
2018-07-13 13:35:22 +0800

    建表动态

--- application/config/_database.php
+++ application/config/_database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'           => 'mysql',
     // 服务器地址
-    'hostname'       => '192.168.1.240',
+    'hostname'       => '127.0.0.1',
     // 数据库名
     'database'       => 'bobo',
     // 用户名
     'username'       => 'root',
     // 密码
-    'password'       => 'xf1980',
+    'password'       => 'gsbb',
     // 端口
     'hostport'       => '3306',
     // 连接dsn
