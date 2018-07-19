9d0a19243ab8a36b3e1dcbae978c6db2411d08be
谢烨 <hatehate123@126.com>
2018-07-19 11:14:50 +0800

    boboshare

--- public/1.php
+++ public/1.php
@@ -1,4 +1,26 @@
 <?php 
-phpinfo();
+class A
+{
+    function display(){
+        echo "this is a A<br>";
+    }
+    
+}
 
-?>
+
+class B
+{
+    function display(){
+        echo "this is a B<br>";
+    }
+    
+}
+
+$aa = new A();
+// $aa->display();
+
+$bb = new B();
+// $bb->display = function (){ echo "in display"; };
+
+
+$bb->display();
