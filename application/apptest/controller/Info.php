f75fa09d3049c1666f905fb6ad7a64b09bce14e2
谢烨 <hatehate123@126.com>
2018-09-05 10:20:26 +0800

    修复bug

---
+++ application/apptest/controller/Info.php
@@ -0,0 +1,30 @@
+<?php
+namespace app\apptest\controller;
+
+use BBExtend\Sys;
+use BBExtend\DbSelect;
+use BBExtend\common\Date;
+use BBExtend\fix\MessageType;
+use think\Db;
+use BBExtend\common\EmojiData;
+use BBExtend\user\Comment;
+
+use think\Config;
+
+class Info 
+{
+   // 
+   public function index()
+   {
+       phpinfo();
+       }
+    
+    
+   
+}
+
+
+
+
+
+
