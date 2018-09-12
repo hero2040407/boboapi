1763eeb0b82380fdb180248df180ba2c0f0f57ad
谢烨 <hatehate123@126.com>
2018-09-12 12:06:08 +0800

    bug fix

--- application/apptest/controller/Temp.php
+++ application/apptest/controller/Temp.php
@@ -5,7 +5,7 @@ use BBExtend\Sys;
 use BBExtend\DbSelect;
 
 use BBExtend\message\Umeng;
-class Temp 
+class Temp extends \think\Controller 
 {
     
    public function index()
@@ -36,7 +36,30 @@ ss;
    }
    
    
-    
+   
+   public function test_redirect(){
+       
+     //  return ['code'=>2];
+       $this->redirect('/apptest/temp/test2');
+       echo 1;
+   }
+   
+   public function test2(){
+       return ['code'=>4];
+   }
+   
+   public function test_redirect2(){
+       
+       //return ['code'=>2];
+       $url = "http://192.168.99.100/apptest/temp/test_redirect";
+       $content = file_get_contents($url);
+//        dump($content);
+        return json_decode( $content);
+       
+       //$this->redirect('/apptest/temp/test2');
+   }
+   
+   
    
 }
 
