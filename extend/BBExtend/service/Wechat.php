a284a469cdb71a4ec32abc928e4441c720828884
谢烨 <hatehate123@126.com>
2018-09-12 17:08:47 +0800

    支付订单生成全局配置

---
+++ extend/BBExtend/service/Wechat.php
@@ -0,0 +1,23 @@
+<?php
+namespace BBExtend\service;
+
+/**
+ * 自己做一个微信服务
+ * 
+ * 让微信各种功能好用一些。
+ * 
+ * @author xieye
+ *
+ */
+class Wechat 
+{
+    
+    public function tongyi_xiadan()
+    {
+        
+        
+    }
+    
+    
+    
+}
