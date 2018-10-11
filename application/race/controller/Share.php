668f98af5b972eef1d951a51267579a51c9ea2bd
lianghuixiang <944517085@qq.com>
2018-10-11 11:25:36 +0800

    修改

--- application/race/controller/Share.php
+++ application/race/controller/Share.php
@@ -8,17 +8,17 @@ use BBExtend\Sys;
 class Share extends Controller
 {
     public $list;
-    
-    
+
+
     public function index(){
         $qd_id = input('?param.id')?input('param.id'):0;
         if($qd_id == 0){
             abort(404,'页面不存在!!请检查路径后再试~');
         }else{
-            
+
             $this->redirect('https://bobo.yimwing.com/webapp/#/contest/dasai/detail/'.$qd_id,302);
-            
-            
+
+
 //             $res = Db::table('ds_race')->where(['id'=>$qd_id])->find();
 //             $race_res = Db::table('ds_race')->where(['id'=>$res['parent']])->find();
 //             if($race_res['start_time'] > time()){
@@ -30,20 +30,20 @@ class Share extends Controller
 //                     $state = 3;//已结束
 //                 }
 //             }
-            
+
 //             $user = Db::table('bb_users')->where(['uid'=>$res['uid']])->find();
 //             echo $this->fetch('',['race_res'=>$race_res,'qd_id'=>$qd_id,'userpic'=>$user['pic'],'state'=>$state]);
         }
     }
-    
-    
+
+
     public function rank($race_id,$startid=0, $length=10,$search='',$self_uid=0)
     {
         $race = \BBExtend\model\Race::find( $race_id );
         if (!$race) {
             return ['code'=>0, 'message' =>'大赛信息错误' ];
         }
-        
+
         $db = Sys::get_container_dbreadonly();
         if ($search) {
             $sql="select id, uid,pic,name,ticket_count from ds_register_log 
@@ -52,7 +52,7 @@ class Share extends Controller
                       and name =? 
                    order by ticket_count desc 
                    limit ?,?";
-            
+
             $user_arr = $db->fetchAll($sql,[ $race_id, $search, $startid, $length ]);
         }else {
             $sql="select id, uid,pic,name,ticket_count from ds_register_log
@@ -60,23 +60,23 @@ class Share extends Controller
                      and has_pay=1
                    order by ticket_count desc,id asc
                    limit ?,?";
-            
+
             $user_arr = $db->fetchAll($sql,[ $race_id,$startid, $length ]);
         }
-        
-        
+
+
         foreach ( $user_arr as $k=>$v ) {
             if ($self_uid) {
-                $user_arr[$k]['my_ticket_count_today'] = $this->get_my_ticket_count_today($race_id, 
+                $user_arr[$k]['my_ticket_count_today'] = $this->get_my_ticket_count_today($race_id,
                         $self_uid, $v['uid']);
-                
-                
+
+
             }else {
                 $user_arr[$k]['my_ticket_count_today'] = 0;
-                
+
             }
             $user_arr[$k]['rank'] = $this->get_rank_by_one($race_id, $v['uid']);
-            
+
         }
         $myinfo=null;
         if ($self_uid) {
@@ -90,37 +90,37 @@ class Share extends Controller
                          'name' =>$row['name'],
                         'ticket_count' =>$row['ticket_count'],
                         'rank' =>$rank,
-                        'my_ticket_count_today' => $this->get_my_ticket_count_today($race_id, 
+                        'my_ticket_count_today' => $this->get_my_ticket_count_today($race_id,
                                 $self_uid, $self_uid),
-                        
-                        
+
+
                 ];
-                
+
             }
-            
+
         }
         $reward='';
         if ( $race->reward ) {
             $reward =  $race->reward;
         }
-        
+
         $sql="select count(*) from  ds_register_log 
                    where zong_ds_id=? 
                      and has_pay=1 ";
         $all_join_count = $db->fetchOne($sql,[ $race_id ]);
-        
-        
+
+
         return ['code'=>1,'data' => [ 'list' =>$user_arr,'myinfo' =>$myinfo ,
                 'all_join_count' =>$all_join_count,
                 'reward' =>$reward] ];
-        
+
     }
-    
-    
-    
+
+
+
     private function get_my_ticket_count_today($race_id,$self_uid, $target_uid)
     {
-        
+
         $db = Sys::get_container_dbreadonly();
         $sql="
 select count(*)   from ds_like
@@ -128,22 +128,21 @@ select count(*)   from ds_like
                      and type=1
                      and self_uid=?
                      and target_uid=?
-                     and datestr=?
                 
 ";
         //$this->list = $join_id_arr = $db->fetchCol($sql,[ $race_id ]);
-        return $db->fetchOne($sql,[ $race_id, $self_uid, $target_uid,date("Ymd") ]);
+        return $db->fetchOne($sql,[ $race_id, $self_uid, $target_uid ]);
     }
-    
-    
-    
+
+
+
     private function get_rank_by_one($race_id,$uid)
     {
         $rank=0;
         if ($this->list) {
-            
+
         }else {
-        
+
           $db = Sys::get_container_dbreadonly();
           $sql="
 select uid   from ds_register_log
@@ -153,9 +152,9 @@ select uid   from ds_register_log
                 
 ";
           $this->list = $join_id_arr = $db->fetchCol($sql,[ $race_id ]);
-          
+
         }
-        
+
         foreach ( $this->list as $all_uid ) {
             $rank++;
             if ( $all_uid==$uid ) {
@@ -163,9 +162,9 @@ select uid   from ds_register_log
             }
         }
         return 0;
-        
+
     }
-    
-    
-    
+
+
+
 }
--- extend/BBExtend/model/UserRace.php
+++ extend/BBExtend/model/UserRace.php
@@ -8,14 +8,14 @@ use BBExtend\video\RaceStatus;
 
 /**
  * 用户中心，与购买有关的类
- * 
+ *
  * User: 谢烨
  */
-class UserRace extends User 
+class UserRace extends User
 {
     public $err_msg='';
     public $success_count =0;
-    
+
     public function like($self_uid, $log_id,$type)
     {
         $db = Sys::get_container_db();
@@ -25,23 +25,19 @@ class UserRace extends User
         $race_id = $row['zong_ds_id'];
         $datestr = date("Ymd");
         $race  = \BBExtend\model\Race::find( $race_id );
-        
+
         if ( time() > $race->end_time  ) {
             $this->success_count=0;
             return true;
         }
-        
-        
 //         $race = Race::find( $row['zong_ds_id'] );
 //         $user = User::find( $uid );
 
         if ($type==1) {
-        
-            
-            $sql="select * from  ds_like where self_uid=? and type=1 and register_log_id=? and datestr=?";
-            $row = $db->fetchRow($sql,[ $self_uid, $log_id, $datestr ]);
+            $sql="select * from  ds_like where self_uid=? and type=1 and register_log_id=?";
+            $row = $db->fetchRow($sql,[ $self_uid, $log_id]);
             if ($row) {
-                $this->err_msg='您今日已投过票，请明天再来';
+                $this->err_msg='您已经投过票,请不要重复投票';
                 return false;
             }
             $bind=[
@@ -53,7 +49,7 @@ class UserRace extends User
                     'count' =>1,
                     'datestr' =>$datestr,
                     'type' =>1,
-                    
+
             ];
             $db->insert("ds_like",$bind);
             $this->success_count=1;
@@ -79,17 +75,17 @@ class UserRace extends User
                     'count' =>1,
                     'datestr' =>$datestr,
                     'type' =>2,
-                    
+
             ];
             $db->insert("ds_like",$bind);
             $this->success_count=1;
             $sql="update ds_register_log set ticket_count = ticket_count+1 where id=?";
             $db->query($sql,[ $log_id ]);
             return true;
-            
-            
+
+
         }
-        
+
         if ($type==3) {
             $result = \BBExtend\Currency::add_bobi($self_uid,
                     -100, '声援大赛好友');
@@ -103,26 +99,26 @@ class UserRace extends User
                         'count' =>1,
                         'datestr' =>$datestr,
                         'type' =>2,
-                        
+
                 ];
                 $db->insert("ds_like",$bind);
                 $this->success_count=1;
                 $sql="update ds_register_log set ticket_count = ticket_count+1 where id=?";
                 $db->query($sql,[ $log_id ]);
                 return true;
-                
+
             }else {
                 $this->err_msg='您的BO币余额不足，不可以投票，请充值';
                 return false;
-                
+
             }
         }
-        
-        
-        
-        
+
+
+
+
         if ($type==4) {
-            
+
                 $bind=[
                         'register_log_id' =>$log_id,
                         'create_time' =>time(),
@@ -132,27 +128,27 @@ class UserRace extends User
                         'count' =>1,
                         'datestr' =>$datestr,
                         'type' =>4,
-                        
+
                 ];
                 $db->insert("ds_like",$bind);
                 $this->success_count=1;
                 $sql="update ds_register_log set ticket_count = ticket_count+1 where id=?";
                 $db->query($sql,[ $log_id ]);
                 return true;
-                
-           
+
+
         }
-        
-        
-        
-        
-        
+
+
+
+
+
         return false;
-        
+
     }
-    
-    
-    
+
+
+
     /**
      * 分享页面
      * @param unknown $log_id
@@ -166,10 +162,10 @@ class UserRace extends User
         $row = $db->fetchRow($sql,[ $log_id ]);
         $uid = $row['uid'];
         $race_id = $row['zong_ds_id'];
-        
+
         $race = Race::find( $row['zong_ds_id'] );
         $user = User::find( $uid );
-        
+
         $result=[];
         $race_info=[];
         $race_info['pic'] = $row['pic'];
@@ -181,69 +177,69 @@ class UserRace extends User
         $race_info['upload_type'] = $race->upload_type;
         $race_info['badge'] = $user->get_badge();
       //  $race_info['upload_checked'] = $row['upload_checked'];
-        
+
         $arr= RaceStatus::get_status_v5($uid, $race_id);
-        
+
         $race_info['status']=  $arr['data']['status'];
-        
+
         $result['race_info'] =$race_info ;
-        
-        
+
+
         $upload=[];
-        
+
         // 下面 是上传内容。
         if ( $race->upload_type == 1 || $race->upload_type == 3) {
             // 视频可能
             if ( $row['record_url'] ) {
-                
+
                 $upload['type']='video';
                 $upload['record_url']=$row['record_url'] ;
                 $upload['record_cover']=$row['record_cover'] ;
-                
+
             }
-            
+
         }else {
             // 图片可能
             if ( $row['pic_id_list'] ) {
-                
-                
+
+
                 $sql="select * from bb_pic where type=1 and uid=?  and act_id=?";
                 $url_arr = $db->fetchAll($sql,[ $uid, $race_id ]);
-                
+
                 if ( $url_arr  ) {
                     $upload['type']='photo';
-                    
+
                     $new=[];
                     foreach ( $url_arr as $v ) {
                         $new[]= [
                                 'url' =>$v['url'],
                                 'pic_width' =>$v['width'],
                                 'pic_height' =>$v['height'],
-                                
+
                         ];
                     }
-                    
+
                     $upload['photo_url_list']=$new ;
                 }
-                
-                
-                
+
+
+
             }
         }
         $result[ 'upload' ] = $upload;
-        
-        
+
+
         // 下面个性，才艺。
-        
+
         $userinfo=[];
         if ( $user->role==3 ) {
             $userinfo['gexing'] = $user->get_gexing_arr();
             $userinfo['jingyan'] = $user->get_jingyan_arr();
-            
+
         }
         $result['vip_info'] = $userinfo;
-        
-        
+
+
         $updates_list = [];
         // 下面 是动态。
         $sql="select id from bb_users_updates
@@ -255,9 +251,9 @@ class UserRace extends User
         $updates_id_arr = $db->fetchCol($sql,[$uid]);
         foreach ($updates_id_arr as $id ) {
             $updates = \BBExtend\model\UserUpdates::find( $id );
-            
+
             $updates_list[]= $updates->list_info(10000);
-            
+
         }
         $result['updates_list'] = $updates_list;
         $count=0;
@@ -265,10 +261,10 @@ class UserRace extends User
             $sql="select count(*) from ds_like where self_uid=? and race_id=? and target_uid=? 
                  and datestr=?";
             $count = $db->fetchOne($sql,[ $self_uid, $race_id, $uid, date("Ymd") ]);
-            
+
         }
         $result['my_ticket_count_today'] = $count;
-        
+
         // 当前用户参加大赛的状态。
         if ($self_uid) {
           $join_arr = \BBExtend\video\RaceStatus::get_status_v5($self_uid, $race_id);
@@ -280,10 +276,10 @@ class UserRace extends User
             $result['self_role'] = $self_user->role;
         }
         return $result;
-        
+
     }
-    
-    
+
+
     private function get_field_name($field_id)
     {
         $db = Sys::get_container_db();
@@ -291,7 +287,7 @@ class UserRace extends User
         $row = $db->fetchRow($sql,[ $field_id ]);
         return $row['title'];
     }
-    
-      
-    
+
+
+
 }
